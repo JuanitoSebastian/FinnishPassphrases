dbd177c59b2998ac59b446a72f19a6a854f5ef85
Juan Covarrubias <me@juan.fi>
2021-05-29 11:31:54 +0300

    Initial Commit

---
+++ FinnishPasswords/FinnishPasswordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinnishPasswordsApp.swift
+//  FinnishPasswords
+//
+//  Created by Juan Covarrubias on 29.5.2021.
+//
+
+import SwiftUI
+
+@main
+struct FinnishPasswordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
