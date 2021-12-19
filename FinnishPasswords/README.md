9da77adf988113245bc1284eea2508b8460023de
Juan Covarrubias <me@juan.fi>
2021-12-19 13:28:47 +0100

    AppState tests now work

---
+++ FinnishPasswords/README.md
@@ -0,0 +1,3 @@
+# FinnishPasswords
+
+A description of this package.
--- FinnishPasswordsTests/AppStateTests.swift
+++ FinnishPasswordsTests/AppStateTests.swift
@@ -12,67 +12,70 @@ import Mockingbird
 class AppStateTests: XCTestCase {
 
     var pasteboard: NSPasteboard!
-    var defaultsStoreMock: DefaultsStoreMock!
-    var passphraseGeneratorMock: PassphraseGeneratorServiceMock!
+    var defaultStore: DefaultsStore!
+    var kotusWordService: KotusWordService!
+    var passsphraseGenerator: PassphraseGeneratorService!
     var appState: AppState!
+    let correctWords = ["ahdasrajaisesti", "ahdasrajaisuus", "ahdata", "ahde", "ahdekaunokki"]
 
     override func setUp() {
-
         let customData = """
-        <st><s>ahdasrajaisesti</s><t><tn>99</tn></t></st>
-        <st><s>ahdasrajaisuus</s><t><tn>40</tn></t></st>
-        <st><s>ahdata</s><t><tn>73</tn><av>F</av></t></st>
-        <st><s>ahde</s><t><tn>48</tn><av>F</av></t></st>
-        <st><s>ahdekaunokki</s></st>
-        """
-
-        self.defaultsStoreMock = mock(DefaultsStore.self).initialize(
+            <st><s>ahdasrajaisesti</s><t><tn>99</tn></t></st>
+            <st><s>ahdasrajaisuus</s><t><tn>40</tn></t></st>
+            <st><s>ahdata</s><t><tn>73</tn><av>F</av></t></st>
+            <st><s>ahde</s><t><tn>48</tn><av>F</av></t></st>
+            <st><s>ahdekaunokki</s></st>
+            """
+        self.pasteboard = NSPasteboard.general
+        self.defaultStore = DefaultsStore(
             userDefaults: UserDefaults(suiteName: #file)!
         )
-        self.passphraseGeneratorMock = mock(PassphraseGeneratorService.self).initialize(
-            kotusWordService: KotusWordService(customData: customData)
+        self.kotusWordService = KotusWordService(customData: customData)
+        kotusWordService.readFileToMemory()
+        self.passsphraseGenerator = PassphraseGeneratorService(
+            kotusWordService: kotusWordService
         )
-
-        self.pasteboard = NSPasteboard.general
-    }
-
-    func test_a_appstate_is_initialized_correctly() {
-//        given(defaultsStoreMock.numberOfWordsInPassphrase)
-//            .willReturn(3)
-//        given(defaultsStoreMock.separatorSymbol)
-//            .willReturn(.hyphen)
-//        given(defaultsStoreMock.wordCapitalization)
-//            .willReturn(false)
-//
-//        let passphraseToReturn = Passphrase(
-//            words: ["ahven", "kirjolohi", "saapas"],
-//            separator: .hyphen
-//        )
-//
-//        given(passphraseGeneratorMock.generatePassphrase(
-//            numOfWords: 3,
-//            separatorSymbol: .hyphen,
-//            wordCapitalization: false
-//        )).willReturn(passphraseToReturn)
-//
-//        appState = AppState(
-//            passphraseGeneratorService: passphraseGeneratorMock,
-//            defaultsStore: defaultsStoreMock,
-//            pasteboard: pasteboard
-//        )
-//
-//        XCTAssertEqual(appState.passphrase, passphraseToReturn)
-    }
-
-}
-
-// MARK: - Supporting functions
-extension AppStateTests {
-    private func initializeAppState() {
         self.appState = AppState(
-            passphraseGeneratorService: passphraseGeneratorMock,
-            defaultsStore: defaultsStoreMock,
+            passphraseGeneratorService: passsphraseGenerator,
+            defaultsStore: defaultStore,
             pasteboard: pasteboard
         )
     }
+
+    func test_a_appstate_is_initialized_correctly() {
+        XCTAssertEqual(appState.passphrase.separator, defaultStore.separatorSymbol)
+        XCTAssertEqual(appState.passphrase.numOfWords, defaultStore.numberOfWordsInPassphrase)
+
+        for word in appState.passphrase.words {
+            XCTAssertTrue(correctWords.contains(word))
+        }
+    }
+
+    func test_b_setting_number_of_words_works() {
+        appState.setNumberOfWordsInPassphrase(5)
+        XCTAssertEqual(defaultStore.numberOfWordsInPassphrase, 5)
+        XCTAssertEqual(appState.numberOfWordsInPassphrase, 5)
+        appState.generateNewPassphrase()
+
+        XCTAssertEqual(appState.passphrase.numOfWords, 5)
+    }
+
+    func test_c_setting_separator_works() {
+        appState.setCurrentSeparator(.slash)
+        XCTAssertEqual(defaultStore.separatorSymbol, .slash)
+        XCTAssertEqual(appState.separatorSymbol, .slash)
+        appState.generateNewPassphrase()
+
+        XCTAssertEqual(appState.passphrase.separator, .slash)
+    }
+
+    func test_d_copying_to_pasteboard_works() {
+        appState.copyPassphraseToPasteboard()
+
+        let textFromPastebaord = pasteboard.pasteboardItems![0]
+            .string(forType: NSPasteboard.PasteboardType(rawValue: "public.utf8-plain-text"))
+
+        XCTAssertEqual(textFromPastebaord, appState.passphrase.passphrase)
+    }
+
 }
