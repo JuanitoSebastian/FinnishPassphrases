65e28337a1a736f75c480792a14cf89f68abdfd9
Juan Covarrubias <me@juan.fi>
2022-03-06 11:41:38 +0200

    New AppIcon, refactoring of code and work on text labels of application

--- FinnishPassphrases/Supporting Files/Constants.swift
+++ FinnishPassphrases/Supporting Files/Constants.swift
@@ -7,8 +7,6 @@
 
 import SwiftUI
 
-let cRoundedCornerSize = CGSize(width: 5, height: 5)
-let cPasswordSeparators: [SeparatorSymbol] = [.asterisk, .hyphen, .slash, .hash]
 let cPassphraseNumberOfWordsRange: ClosedRange<Double> = 3...10
 let cPassphraseNumberOfWordsRangeInt: ClosedRange = 3...10
 let cProbabilityOfCapitalizedString: Double = 0.5
